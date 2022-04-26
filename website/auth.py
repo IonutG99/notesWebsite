3da1382ed899f5d35cb9b09b3927e95f1d60fcad
IonutG99 <ionutghimici@yahoo.com>
2022-04-26 10:56:21 +0300

    Project added.

---
+++ website/auth.py
@@ -0,0 +1,19 @@
+# store the roots
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
+
+
+@auth.route('/sign-up')
+def sign_up():
+    pass
