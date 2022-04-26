faccb878f1d8ee4d563db30b54f8e991fc189484
IonutG99 <ionutghimici@yahoo.com>
2022-04-26 23:14:06 +0300

    Project v1.1.

--- website/auth.py
+++ website/auth.py
@@ -38,27 +38,28 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
         user = User.query.filter_by(email=email).first()
         if user:
-            flash('Email already exists!', category='error')
+            flash('Email already exists.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\' t match.', category='error')
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=firstName,
-                            password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
